9e43e66371fb434aac97b39a87d8e63304299e11
NBRosati <n.busto@hotmail.com>
2022-06-20 21:05:09 -0300

    broma agregada

--- js/calc.js
+++ js/calc.js
@@ -1,18 +1,28 @@
-
-//para la pantalla
-function dis(val)
-{
-document.getElementById("calcu").value+=val
- }
-//evaluacion
-function solve()
-{
-let x = document.getElementById("calcu").value
-let y = eval(x)
-document.getElementById("calcu").value = y
+// Para la pantalla
+function appendNumber(val) {
+    document.getElementById("calcu").value += val
 }
-//borra pantalla
-function clr()
-{
-document.getElementById("calcu").value = ""
+
+// Evaluacion
+function solveOperation() {
+    let x = document.getElementById("calcu").value
+    let y = eval(x)
+
+    if (x === '2+2') {
+        joke()
+    }
+    
+    document.getElementById("calcu").value = y
+}
+
+// Borra pantalla
+function clearScreen() {
+    document.getElementById("calcu").value = ""
+}
+
+// Muestra broma
+function joke() {
+    if (window.confirm("A primaria")) {
+        window.open('https://www.niche.com/k12/search/best-public-elementary-schools/','_blank');
+    }
 }
